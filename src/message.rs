35b718d8391dda76ed753fedaf0e518472f9c827
Hubert Deng <hubertdeng123@gmail.com>
2023-08-22 12:49:26 -0700

    add ignore logic

--- src/message.rs
+++ src/message.rs
@@ -1,9 +1,13 @@
+extern crate regex;
+
 use std::{path::PathBuf, collections::HashSet};
 
 use anyhow::Error;
 use git2::Commit;
+use regex::Regex;
 
 /// Describes a commit-message level `CHANGE_TOGETHER_IGNORE` statement.
+#[derive(PartialEq, Hash, Eq, Debug)]
 pub(crate) enum Ignore<'a> {
     /// A commit-message ignore like `CHANGE_TOGETHER_IGNORE=/path/to/my/file`
     File(PathBuf),
@@ -19,6 +23,40 @@ pub(crate) enum Ignore<'a> {
 }
 
 pub(crate) fn get_ignores<'a>(commit: &'a Commit) -> Result<HashSet<Ignore<'a>>, Error> {
-    // TODO: fill in
-    todo!()
+    let commit_message = commit.message().unwrap_or("");
+    let pattern = r"CHANGE_TOGETHER_IGNORE=([^\s]+)";
+    let re = Regex::new(pattern).unwrap();
+
+    let mut ignore_list: HashSet<Ignore> = HashSet::new();
+
+    // Iterate through all matches in the commit message and collect them
+    // TODO: add error handling
+    for captures in re.captures_iter(commit_message) {
+        if let Some(matched_text) = captures.get(1) {
+            let path_and_tag: &'a str  = matched_text.as_str();
+            if path_and_tag == "*" {
+                let mut hash_set_with_all: HashSet<Ignore> = HashSet::new();
+                hash_set_with_all.insert(Ignore::All());
+                return Ok(hash_set_with_all);
+            }
+            else if path_and_tag.contains("/") && path_and_tag.contains("#") {
+                let parts: Vec<&str> = path_and_tag.split("#").collect();
+                let path = parts[0];
+                let tag = parts[1];
+                ignore_list.insert(Ignore::FileTag(PathBuf::from(path), tag));
+            }
+            else if path_and_tag.contains("#") {
+                let parts: Vec<&str> = path_and_tag.split("#").collect();
+                let tag = parts[1];
+                ignore_list.insert(Ignore::Tag(tag));
+            }
+            else if path_and_tag.contains("/") {
+                ignore_list.insert(Ignore::File(PathBuf::from(path_and_tag)));
+            }
+            else {
+                return Err("Error parsing string")
+            }
+        }
+    }
+    Ok(ignore_list)
 }
--- src/startup.rs
+++ src/startup.rs
@@ -3,7 +3,7 @@ extern crate git2;
 use anyhow::{Error, Context};
 use git2::Repository;
 
-pub fn clone_repo(repo: String) -> Result<Repository, Error> {
+pub(crate) fn clone_repo(repo: String) -> Result<Repository, Error> {
     let url = format!("https://github.com/{}", repo);
     Repository::clone(&url, format!("/tmp/{}", repo)).context("failed to clone")
 }
